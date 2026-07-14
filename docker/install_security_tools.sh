b6f4550cbc77a468c84415c5358ae0a6d5fd2e0f
yashw1504 <yash.wankar@sonata-software.com>
2026-07-14 12:55:06 +0530

    re

--- docker/install_security_tools.sh
+++ docker/install_security_tools.sh
@@ -1,153 +1,88 @@
 #!/bin/bash
+
 set -e
 
 echo "=================================="
 echo "Installing Security Tools"
 echo "=================================="
 
-# -------------------------------
-# Load tool versions
-# -------------------------------
-source /tmp/tool_versions.env
-
-# -------------------------------
-# Install OS packages
-# -------------------------------
-apt-get update
-
-DEBIAN_FRONTEND=noninteractive apt-get install -y \
-    wget \
-    curl \
-    unzip \
-    tar \
-    git \
-    ca-certificates \
-    python3-pip
-
-rm -rf /var/lib/apt/lists/*
-
-# -------------------------------
+####################################
 # Nuclei
-# -------------------------------
-echo "Installing Nuclei ${NUCLEI_VERSION}"
+####################################
 
-wget -q \
-https://github.com/projectdiscovery/nuclei/releases/download/v${NUCLEI_VERSION}/nuclei_${NUCLEI_VERSION}_linux_amd64.zip
+wget https://github.com/projectdiscovery/nuclei/releases/download/v3.4.7/nuclei_3.4.7_linux_amd64.zip
 
-unzip -q nuclei_${NUCLEI_VERSION}_linux_amd64.zip
+unzip nuclei_3.4.7_linux_amd64.zip
 
 mv nuclei /usr/local/bin/
 
 chmod +x /usr/local/bin/nuclei
 
-rm nuclei_${NUCLEI_VERSION}_linux_amd64.zip
+rm nuclei_3.4.7_linux_amd64.zip
 
-# -------------------------------
+
+####################################
 # Semgrep
-# -------------------------------
-echo "Installing Semgrep"
+####################################
 
-pip3 install --no-cache-dir \
-semgrep==${SEMGREP_VERSION}
+pip install semgrep
 
-# -------------------------------
+
+####################################
 # Checkov
-# -------------------------------
-echo "Installing Checkov"
+####################################
 
-pip3 install --no-cache-dir \
-checkov==${CHECKOV_VERSION}
+pip install checkov
 
-# -------------------------------
+
+####################################
 # Syft
-# -------------------------------
-echo "Installing Syft ${SYFT_VERSION}"
+####################################
 
-curl -sSfL \
-https://raw.githubusercontent.com/anchore/syft/main/install.sh \
-| sh -s -- \
--b /usr/local/bin \
-v${SYFT_VERSION}
+curl -sSfL https://raw.githubusercontent.com/anchore/syft/main/install.sh \
+| sh -s -- -b /usr/local/bin
 
-# -------------------------------
+
+####################################
 # Grype
-# -------------------------------
-echo "Installing Grype ${GRYPE_VERSION}"
+####################################
 
-curl -sSfL \
-https://raw.githubusercontent.com/anchore/grype/main/install.sh \
-| sh -s -- \
--b /usr/local/bin \
-v${GRYPE_VERSION}
+curl -sSfL https://raw.githubusercontent.com/anchore/grype/main/install.sh \
+| sh -s -- -b /usr/local/bin
 
-# -------------------------------
+
+####################################
 # Trivy
-# -------------------------------
-echo "Installing Trivy ${TRIVY_VERSION}"
+####################################
 
-wget -q \
-https://github.com/aquasecurity/trivy/releases/download/v${TRIVY_VERSION}/trivy_${TRIVY_VERSION}_Linux-64bit.deb
+wget https://github.com/aquasecurity/trivy/releases/latest/download/trivy_0.67.2_Linux-64bit.deb
 
-dpkg -i \
-trivy_${TRIVY_VERSION}_Linux-64bit.deb
+dpkg -i trivy_0.67.2_Linux-64bit.deb
 
-rm \
-trivy_${TRIVY_VERSION}_Linux-64bit.deb
+rm trivy_0.67.2_Linux-64bit.deb
 
-# -------------------------------
+
+####################################
 # Gitleaks
-# -------------------------------
-echo "Installing Gitleaks ${GITLEAKS_VERSION}"
+####################################
 
-wget -q \
-https://github.com/gitleaks/gitleaks/releases/download/v${GITLEAKS_VERSION}/gitleaks_${GITLEAKS_VERSION}_linux_x64.tar.gz
+wget https://github.com/gitleaks/gitleaks/releases/latest/download/gitleaks_8.28.0_linux_x64.tar.gz
 
-tar -xzf \
-gitleaks_${GITLEAKS_VERSION}_linux_x64.tar.gz
+tar -xzf gitleaks_8.28.0_linux_x64.tar.gz
 
 mv gitleaks /usr/local/bin/
 
 chmod +x /usr/local/bin/gitleaks
 
-rm \
-gitleaks_${GITLEAKS_VERSION}_linux_x64.tar.gz
+rm gitleaks_8.28.0_linux_x64.tar.gz
 
-# -------------------------------
-# OSV Scanner
-# -------------------------------
-echo "Installing OSV Scanner ${OSV_SCANNER_VERSION}"
 
-wget -q \
-https://github.com/google/osv-scanner/releases/download/v${OSV_SCANNER_VERSION}/osv-scanner_linux_amd64
-
-mv osv-scanner_linux_amd64 /usr/local/bin/osv-scanner
-
-chmod +x /usr/local/bin/osv-scanner
-
-# -------------------------------
+####################################
 # Update Nuclei Templates
-# -------------------------------
-echo "Updating Nuclei Templates"
+####################################
 
-nuclei -update-templates || true
+nuclei -update-templates
 
-# -------------------------------
-# Verify installations
-# -------------------------------
-echo
-echo "Installed Versions"
-echo "------------------"
-
-nuclei -version || true
-semgrep --version || true
-checkov --version || true
-syft version || true
-grype version || true
-trivy --version || true
-gitleaks version || true
-osv-scanner --version || true
-
-echo
 echo "=================================="
-echo "Security Tools Installed Successfully"
+echo "Installation Completed"
 echo "=================================="
